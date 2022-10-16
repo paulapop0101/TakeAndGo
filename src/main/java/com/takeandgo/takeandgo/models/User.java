21a687ebfc406b1962cae1b41dbb2ba5ef17a6be
poppaula <poppaula0106@gmail.com>
2022-10-16 16:22:57 +0300

    'user'

---
+++ src/main/java/com/takeandgo/takeandgo/models/User.java
@@ -0,0 +1,27 @@
+package com.takeandgo.takeandgo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+}
