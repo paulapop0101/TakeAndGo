e2e129d925e8af44c1fe91e47d440a25694749d3
flavia <flaviamioc@yahoo.com>
2022-10-16 16:56:27 +0300

    Added Card class and it's foreign keys

---
+++ src/main/java/com/takeandgo/takeandgo/models/Card.java
@@ -0,0 +1,31 @@
+package com.takeandgo.takeandgo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String IBAN;
+
+    private Timestamp expDate;
+
+    private int CVV;
+
+    @ManyToOne
+    @JoinColumn(name = "card_id", referencedColumnName = "id")
+    private User user;
+
+}
