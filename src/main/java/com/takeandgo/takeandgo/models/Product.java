4033236e3d6ba063050e506ccfbef0b5e1833307
flavia <flaviamioc@yahoo.com>
2022-10-16 17:09:08 +0300

    Added Product class

--- src/main/java/com/takeandgo/takeandgo/models/Product.java
+++ src/main/java/com/takeandgo/takeandgo/models/Product.java
@@ -1,4 +1,22 @@
 package com.takeandgo.takeandgo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String barcode;
+    private  float price;
+
 }
