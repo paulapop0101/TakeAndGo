ec6820f9978a1ce37dc0069e335a997bb3c023ee
poppaula <poppaula0106@gmail.com>
2022-10-16 15:40:41 +0300

    'first'

---
+++ src/main/java/com/takeandgo/takeandgo/TakeandgoApplication.java
@@ -0,0 +1,13 @@
+package com.takeandgo.takeandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeandgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeandgoApplication.class, args);
+	}
+
+}
