7b7d835e997997d5f59be677b3bd6b191ed948d0
poppaula <poppaula0106@gmail.com>
2022-10-20 21:53:13 +0300

    'pom xml'

--- src/main/java/com/takeandgo/takeandgo/repositories/UserRepository.java
+++ src/main/java/com/takeandgo/takeandgo/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.takeandgo.takeandgo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
