89d36ce7b09c5b0e351c56713ab36815aa627658
poppaula <poppaula0106@gmail.com>
2022-11-21 18:57:04 +0200

    'usercreateDTO'

---
+++ src/main/java/com/takeandgo/takeandgo/dtos/UserCreateDTO.java
@@ -0,0 +1,15 @@
+package com.takeandgo.takeandgo.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDTO {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
