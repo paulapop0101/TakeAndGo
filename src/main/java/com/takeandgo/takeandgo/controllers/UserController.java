c6615d885787a238cf030758d7d1ddbc637ff75c
poppaula <poppaula0106@gmail.com>
2023-01-23 14:38:04 +0200

    small changes again

--- src/main/java/com/takeandgo/takeandgo/controllers/UserController.java
+++ src/main/java/com/takeandgo/takeandgo/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8100","http://localhost:8080","http://localhost:4200","http://192.168.1.103:4200"})
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
